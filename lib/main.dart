94ea517cdf9a1865925fdfccad5ecad52d77f537
mebuki117 <0yamelon@gmail.com>
2024-10-24 01:15:54 +0900

    version bump

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class Main extends StatefulWidget {
 class MainState extends State<Main> {
   static const platform = MethodChannel('com.example.pace_alert/service');
 
-  final String currentVersion = '1.3.2';
+  final String currentVersion = '1.3.3';
   Map<String, String>? _updateInfo;
   bool _isUpdateChecked = false;
 
