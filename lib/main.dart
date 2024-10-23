2f9e0c62cceef2aa91ffdd1064bdf4598d2b5dc1
mebuki117 <0yamelon@gmail.com>
2024-10-23 23:10:34 +0900

    add stats features

--- lib/main.dart
+++ lib/main.dart
@@ -34,21 +34,34 @@ class Main extends StatefulWidget {
 class MainState extends State<Main> {
   static const platform = MethodChannel('com.example.pace_alert/service');
 
+  final String currentVersion = '1.2.0';
+  Map<String, String>? _updateInfo;
+  bool _isUpdateChecked = false;
+
   List<dynamic> _data = [];
+  List<dynamic> _statsdata = [];
   Timer? _timer;
+
   FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
   Map<String, Set<String>> notifiedEventIds = {};
   Set<int> sentNotificationIds = {};
   bool _isLiveOnly = false;
-  final String currentVersion = '1.2.0';
-  Map<String, String>? _updateInfo;
-  bool _isUpdateChecked = false;
+
+  int _selectedIndex = 0;
+  int _selectedTabIndex = 0;
+  String _selectedType = 'Enter';
+  String _selectedDays = '30';
+  String _selectedLimit = '10';
+
+  String defaultStats =
+      'getLeaderboard?category=nether&type=count&days=30&limit=10';
 
   @override
   void initState() {
     super.initState();
     startForegroundService();
     fetchData();
+    fetchStatsData(defaultStats);
     _timer = Timer.periodic(const Duration(seconds: 20), (timer) {
       fetchData();
     });
@@ -113,13 +126,39 @@ class MainState extends State<Main> {
     }
   }
 
-  String formatTime(int time) {
+  Future<void> fetchStatsData(String endpoint) async {
+    try {
+      final response =
+          await http.get(Uri.parse('https://paceman.gg/stats/api/$endpoint'));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          _statsdata = json.decode(response.body);
+        });
+        log('Stats Data fetched successfully: ${_statsdata.length} items.');
+        log('send point: ${endpoint}');
+      } else {
+        throw Exception('Failed to load stats data');
+      }
+    } catch (e) {
+      log('Error fetching stats data: $e');
+    }
+  }
+
+  String formatTime(int time, {bool includeDecimal = false}) {
     int seconds = time ~/ 1000;
+    int milliseconds = time % 1000;
 
     int minutes = seconds ~/ 60;
     int remainingSeconds = seconds % 60;
 
-    return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
+    int decimalPart = (milliseconds ~/ 100) % 10;
+
+    if (includeDecimal) {
+      return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}.$decimalPart';
+    } else {
+      return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
+    }
   }
 
   @override
@@ -159,8 +198,246 @@ class MainState extends State<Main> {
     'rsg.enter_nether': 7,
   };
 
+  final List<String> _tabs = [
+    'Nether',
+    'Structure 1',
+    'Structure 2',
+    'First Portals',
+    'Stronghold',
+    'End',
+    'Completion',
+  ];
+
+  final List<String> _tabImages = [
+    'assets/icons/nether.png',
+    'assets/icons/bastion.png',
+    'assets/icons/fortress.png',
+    'assets/icons/portal.png',
+    'assets/icons/sh.png',
+    'assets/icons/end.png',
+    'assets/icons/credits.png',
+  ];
+
+  void _fetchDataForCurrentTab(int index) {
+    String category = '';
+
+    switch (index) {
+      case 0:
+        category = 'nether';
+        break;
+      case 1:
+        category = 'first_structure';
+        break;
+      case 2:
+        category = 'second_structure';
+        break;
+      case 3:
+        category = 'first_portal';
+        break;
+      case 4:
+        category = 'stronghold';
+        break;
+      case 5:
+        category = 'end';
+        break;
+      case 6:
+        category = 'finish';
+        break;
+      default:
+        category = 'nether';
+    }
+
+    String daysValue;
+    switch (_selectedDays) {
+      case '1':
+        daysValue = '1';
+        break;
+      case '7':
+        daysValue = '7';
+        break;
+      case '30':
+        daysValue = '30';
+        break;
+      case '9999':
+        daysValue = '9999';
+        break;
+      default:
+        daysValue = '30';
+    }
+
+    String limitValue;
+    switch (_selectedLimit) {
+      case '10':
+        limitValue = '10';
+        break;
+      case '30':
+        limitValue = '30';
+        break;
+      default:
+        limitValue = '10';
+    }
+
+    fetchStatsData(
+        'getLeaderboard?category=$category&type=${_selectedType == 'Qty' ? 'count' : _selectedType.toLowerCase()}&days=$daysValue&limit=$limitValue');
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTabController(
+          length: _tabs.length,
+          child: Column(
+            children: [
+              if (_selectedIndex == 1)
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DropdownButton<String>(
+                          value: _tabs[_selectedTabIndex],
+                          items: List.generate(_tabs.length, (index) {
+                            return DropdownMenuItem<String>(
+                              value: _tabs[index],
+                              child: Row(
+                                children: [
+                                  Image.asset(
+                                    _tabImages[index],
+                                    height: 24,
+                                    width: 24,
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Text(_tabs[index]),
+                                ],
+                              ),
+                            );
+                          }),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedTabIndex = _tabs.indexOf(newValue!);
+                              _fetchDataForCurrentTab(_selectedTabIndex);
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DropdownButton<String>(
+                          value: _selectedType,
+                          items: [
+                            'Enter',
+                            'Average',
+                            'Fastest',
+                          ].map((String type) {
+                            return DropdownMenuItem<String>(
+                              value: type,
+                              child: Text(
+                                type,
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedType = newValue!;
+                              _fetchDataForCurrentTab(_selectedTabIndex);
+                            });
+                          },
+                        ),
+                        DropdownButton<String>(
+                          value: _selectedDays,
+                          items: [
+                            '1',
+                            '7',
+                            '30',
+                            '9999',
+                          ].map((String day) {
+                            return DropdownMenuItem<String>(
+                              value: day,
+                              child: Text(
+                                day == '1'
+                                    ? '24 hours'
+                                    : day == '7'
+                                        ? '7 days'
+                                        : day == '30'
+                                            ? '30 days'
+                                            : 'Lifetime',
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedDays = newValue!;
+                              _fetchDataForCurrentTab(_selectedTabIndex);
+                            });
+                          },
+                        ),
+                        DropdownButton<String>(
+                          value: _selectedLimit,
+                          items: [
+                            '10',
+                            '30',
+                          ].map((String limit) {
+                            return DropdownMenuItem<String>(
+                              value: limit,
+                              child: Text(
+                                limit == '10' ? 'TOP 10' : 'TOP 30',
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedLimit = newValue!;
+                              _fetchDataForCurrentTab(_selectedTabIndex);
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              Expanded(
+                child: _selectedIndex == 0
+                    ? _buildCurrentPaceView()
+                    : _selectedIndex == 1
+                        ? _buildStatsView()
+                        : _buildSettingsView(),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run),
+            label: 'Current Pace',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Stats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildCurrentPaceView() {
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -188,30 +465,6 @@ class MainState extends State<Main> {
                   ],
                 ),
               ),
-            AppBar(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      const Text('Current Pace'),
-                      const SizedBox(width: 8),
-                      Image.asset(
-                        'assets/icons/paceman.png',
-                        width: 24,
-                        height: 24,
-                      ),
-                    ],
-                  ),
-                  Text(
-                    'v$currentVersion',
-                    style: const TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
-              ),
-            ),
             Expanded(
               child: Column(
                 children: <Widget>[
@@ -386,7 +639,7 @@ class MainState extends State<Main> {
                                                         style: const TextStyle(
                                                           color: Colors.grey,
                                                         ),
-                                                      )
+                                                      ),
                                                     ],
                                                     if (itemData?[
                                                             'minecraft:blaze_rod'] !=
@@ -426,6 +679,103 @@ class MainState extends State<Main> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildStatsView() {
+    if (_statsdata.isEmpty) {
+      return const Center(
+        child: Text('No stats available'),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: _statsdata.length,
+      itemBuilder: (context, index) {
+        final statsItem = _statsdata[index];
+
+        final int qtyValue = statsItem['qty'];
+        final int avgValue = statsItem['avg'].floor();
+        final formattedTime = formatTime(avgValue, includeDecimal: true);
+
+        final String selectedType = _selectedType;
+
+        final Color qtyColor =
+            (selectedType == 'Average' || selectedType == 'Fastest')
+                ? Colors.grey
+                : (Theme.of(context).brightness == Brightness.dark
+                    ? Colors.white
+                    : Colors.black);
+
+        final Color avgColor = (selectedType == 'Enter')
+            ? Colors.grey
+            : (Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Colors.black);
+
+        final String playerName = statsItem['name'] ?? 'No Name';
+        final String playerUrl = 'https://paceman.gg/stats/player/$playerName';
+
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          elevation: 4,
+          child: ListTile(
+            dense: true,
+            contentPadding: const EdgeInsets.all(16),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    launchUrl(Uri.parse(playerUrl));
+                  },
+                  child: Text(
+                    '${index + 1}. $playerName',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '$qtyValue',
+                      style: TextStyle(
+                        color: qtyColor,
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      formattedTime,
+                      style: TextStyle(
+                        color: avgColor,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSettingsView() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'There is nothing hrere yet :(',
+          ),
+          Text(
+            'v$currentVersion',
+          ),
+        ],
       ),
     );
   }
