4353701d18ddfb2457f0d7a1a06c07d5834bd85b
mebuki117 <0yamelon@gmail.com>
2024-10-25 12:33:25 +0900

    version bump

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class Main extends StatefulWidget {
 class MainState extends State<Main> {
   static const platform = MethodChannel('com.example.pace_alert/service');
 
-  final String currentVersion = '1.3.3';
+  final String currentVersion = '1.4.0';
   Map<String, String>? _updateInfo;
   bool _isUpdateChecked = false;
 
@@ -58,7 +58,7 @@ class MainState extends State<Main> {
   String defaultStats =
       'getLeaderboard?category=nether&type=count&days=30&limit=10';
 
-  TextEditingController _searchController = TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
 
   bool _isSearchMode = false;
   String _searchDays = '720';
